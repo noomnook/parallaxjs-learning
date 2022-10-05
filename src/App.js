edbc580c81e542aedf05c94da80a244bfee8b475
NOOK <81733159+noomnook@users.noreply.github.com>
2022-10-05 16:20:02 +0700

    parallax

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './components/Home'
+import '/node_modules/primeflex/primeflex.css'
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+function App() {
+  return (
+      <Home />
+  );
+}
+
+export default App;
